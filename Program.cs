69170bbc4945519afe3635cb407da7d99349ab6b
Lauren McCall <lmmccolloch@gmail.com>
2021-12-09 10:41:43 -0500

    value method working

--- Program.cs
+++ Program.cs
@@ -8,7 +8,55 @@ namespace BlackJackCS
         // properties of class Card: suit, rank, and value
         public string Suit { get; set; }
         public string Rank { get; set; }
-        public int Value { get; set; }
+
+        public int Value()
+        {
+            // if 2-10 --> value = 2-10
+            if (Rank == "2")
+            {
+                return 2;
+            }
+            if (Rank == "3")
+            {
+                return 3;
+            }
+            if (Rank == "4")
+            {
+                return 4;
+            }
+            if (Rank == "5")
+            {
+                return 5;
+            }
+            if (Rank == "6")
+            {
+                return 6;
+            }
+            if (Rank == "7")
+            {
+                return 7;
+            }
+            if (Rank == "8")
+            {
+                return 8;
+            }
+            if (Rank == "9")
+            {
+                return 9;
+            }
+            // if J, Q, K --> value = 10
+            if (Rank == "10" || Rank == "Jack" || Rank == "Queen" || Rank == "King")
+            {
+                return 10;
+            }
+            // if Ace --> value = 11
+            if (Rank == "Ace")
+            {
+                return 11;
+            }
+            // default check -- c# won't run without this. 
+            return 0;
+        }
 
         override public string ToString()
         {
